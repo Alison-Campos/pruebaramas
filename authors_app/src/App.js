fa43f630486e420e4b851734dad630fa1a29fbaa
Alison-Campos <89162847+Alison-Campos@users.noreply.github.com>
2024-04-09 13:01:52 -0600

    cambio

--- authors_app/src/App.js
+++ authors_app/src/App.js
@@ -1,26 +1,3 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';import AuthorsList from './componentes/AuthorsList';
-import Header from './componentes/Header';
 
-import PgAutorInsert from './pages/insertAutores.jsx';
-
-function App() {
-  return (
-    <div>
-      <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<AuthorsList />} />
-          <Route path="/insertAutores" element={<PgAutorInsert />} />
-        </Routes>
-      </div>
-    </Router>
-
-      
-    </div>
-  );
-}
-
-export default App;
 
 
